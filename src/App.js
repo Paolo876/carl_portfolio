6c7078c2803c4de77146556c12ddd535f60df4fe
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-21 23:12:57 -0700

    updated home page ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import bg from "./assets/bg-white.png"
+// import bg from "./assets/bg-white.png"
 import { BrowserRouter } from "react-router-dom";
 import Navbar from './components/layout/Navbar';
 import MainNavigation from './components/layout/MainNavigation';
 import { useAuthContext } from './hooks/useAuthContext';
 import DevNavbar from './pages/dev/DevNavbar';
+
 function App() {
   const { user } = useAuthContext();
   return (
-    <div className="app" style={{background: `url(${bg})`}}>
+    <div className="app" >
       <BrowserRouter>
         { user && <DevNavbar/> }
         <Navbar/>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,18 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import "./Home.scss";
 import Landing from './Landing';
 import Works from "./Works";
 import bg from "../../assets/bg-white.png";
-import { useIntersection } from '../../hooks/useIntersection';
 import About from './About';
+import { Box, Stack } from '@mui/material';
 
 export default function Home() {
-  const worksRef = useRef();
-  const Aboutref = useRef();
+  // const worksRef = useRef();
+  // const Aboutref = useRef();
   // const inViewport = useIntersection(worksRef, '-30%'); // Trigger as soon as the element becomes visible
 
   return (
-    <div className='home' style={{background: `url(${bg})`}}>
+    <Box className='home'>
       <Landing/>
       <Works/>
       <About/>
@@ -21,6 +21,6 @@ export default function Home() {
       <footer>
         <p>© 2022 Carl Adriant Dimabuyu, All Rights Reserved.</p>
       </footer>
-    </div>
+    </Box>
   )
 }
