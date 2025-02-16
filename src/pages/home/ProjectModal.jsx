019c0dfaa272d2e43a05b6ab59533c3b9b1b00dc
paoloBugarin <paolopaolobugarin@gmail.com>
2025-02-15 16:05:21 -0800

    updated modal ui

--- src/pages/home/ProjectModal.jsx
+++ src/pages/home/ProjectModal.jsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from 'react';
-import { Modal, Typography, Box, Backdrop, Fade, List, ListItem, Button, IconButton, MobileStepper, SpeedDial } from '@mui/material'
+import { Modal, Typography, Box, Backdrop, Fade, List, ListItem, IconButton, MobileStepper, Portal } from '@mui/material'
 import Image from 'mui-image'
 import useProjectsRedux from '../../hooks/useProjectsRedux';
-import Carousel from 'react-material-ui-carousel'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 
 
 const style = {
@@ -21,16 +19,16 @@ const style = {
   height: "max-content",
   // width: "100%",
   width: "max-content",
-  maxHeight: "92vh",
-  maxWidth: "1500px",
+  maxHeight: {md: "85vh", lg:"92vh"},
+  maxWidth: { sm: "400px", md: "700px", lg: "1000px", xl:"1500px" },
   outline: "none"
 };
 
 const containerProps = {
   display: "grid",
-  gridTemplateAreas: "'image desc'",
+  gridTemplateAreas: {md: "'image''desc'", lg:"'image desc'"},
   // gridTemplateColumns: `repeat(auto-fill, minmax(280px, 1fr))`,
-  gridTemplateColumns: `minmax(auto, 1fr) 380px`,
+  gridTemplateColumns: {lg: `minmax(auto, 1fr) 300px`, xl:`minmax(auto, 1fr) 380px`},
   alignItems: "center"
 }
 
@@ -41,7 +39,7 @@ const imageProps = {
   minHeight: "auto",
   minWidth: "auto",
   maxHeight: "90vh",
-  maxWidth: "90vw",
+  maxWidth: "85vw",
   position: "relative",
   userSelect: "none"
 }
@@ -50,34 +48,35 @@ const imageProps = {
 const headerTextProps = {
   fontWeight: 500,
   letterSpacing: 1,
-  fontSize: 20,
+  fontSize: {md: 17, lg: 18, xl:20},
   borderBottom: 1,
   borderColor: "primary.main",
+  pb: {md: .2, lg: 0 }
 }
 
 const subHeaderTextProps = {
   fontWeight: 300,
   letterSpacing: 1,
-  fontSize: 13,
+  fontSize: {md: 12, xl:13},
   opacity: .85,
-  textAlign: "right",
+  textAlign: {lg:"right"},
   pr: 1,
 }
 
 const listProps = {
   m:0,
   p:0,
-  mt: 10
+  mt: {sm: 3, md: 4, lg:10},
 }
 
 const listItemProps = {
   m:0,
   p:0,
-  mt: .85,
+  mt: {md: .5, lg:.85},
   fontWeight: 300,
   letterSpacing: 1,
-  fontSize: 12,
-  opacity: .8
+  fontSize: {md: 11, lg: 11, xl: 12},
+  opacity: .8,
 }
 
 const buttonContainerProps = {
@@ -115,17 +114,27 @@ const ProjectModal = ({ setIsModalVisible, isModalVisible }) => {
   const { projects } = useProjectsRedux();
   const [ selectedImage, setSelectedImage ] = useState(null)
   const [ selectedImageIdx, setSelectedImageIdx ] = useState(0);
+
   useEffect(() => {
-    setSelectedImage(projects.find(item => item.id === id))
+    setSelectedImage(({...projects.find(item => item.id === id), index: projects.findIndex(item => item.id === id)}))
   },[])
 
 
-  const handleBackClick = () => {
+  const handleImageBackClick = () => {
     if(selectedImageIdx !== 0) setSelectedImageIdx(selectedImageIdx - 1)
   }
 
-  const handleNextClick = () => {
+  const handleImageNextClick = () => {
     if(selectedImageIdx < selectedImage.images.length - 1) setSelectedImageIdx(selectedImageIdx + 1)
+  }
+
+    
+  const handleProjectBackClick = () => {
+    if(selectedImage.index !== 0) setSelectedImage(({...projects[selectedImage.index - 1], index: selectedImage.index - 1}))
+  }
+
+  const handleProjectNextClick = () => {
+    if(selectedImage.index < projects.length - 1) setSelectedImage(({...projects[selectedImage.index + 1], index: selectedImage.index + 1}))
   }
 
 
@@ -156,10 +165,10 @@ const ProjectModal = ({ setIsModalVisible, isModalVisible }) => {
                 {<Image src={selectedImage.images[selectedImageIdx].src} alt={selectedImage.images[selectedImageIdx].filename} duration={100} sx={imageProps} fit="cover" draggable={false}/>}
                 {/* buttons */}
                 {selectedImage.images.length > 1 && <Box sx={buttonContainerProps}>
-                  <IconButton onClick={handleBackClick} sx={{visibility: selectedImageIdx === 0 ? "hidden" : "visible"}}>
+                  <IconButton onClick={handleImageBackClick} sx={{visibility: selectedImageIdx === 0 ? "hidden" : "visible"}}>
                     <ArrowBackIosNewIcon sx={buttonProps}/>
                   </IconButton>
-                  <IconButton onClick={handleNextClick} sx={{visibility: selectedImageIdx >= selectedImage.images.length - 1 ? "hidden" : "visible"}}>
+                  <IconButton onClick={handleImageNextClick} sx={{visibility: selectedImageIdx >= selectedImage.images.length - 1 ? "hidden" : "visible"}}>
                     <ArrowForwardIosIcon sx={buttonProps}/>
                   </IconButton>
                   <Box
@@ -193,7 +202,7 @@ const ProjectModal = ({ setIsModalVisible, isModalVisible }) => {
                   </Box>
                 </Box>}
               </Box>
-              <Box sx={{gridArea: "desc", bgcolor: 'background.paper', py: 5, pl: 1.5, pr: 3,}}>
+              <Box sx={{gridArea: "desc", bgcolor: 'background.paper', py: {xs: 1, sm: 1.5, md: 2, lg:5}, pl: 1.5, pr: 3,}}>
                 <Typography id="project-image-modal-text" variant="h6" sx={headerTextProps}>{selectedImage.header}</Typography>
                 <Typography id="project-image-modal-text" variant="h6" sx={subHeaderTextProps}>{selectedImage.style}</Typography>
                 <List sx={listProps}>
@@ -201,23 +210,28 @@ const ProjectModal = ({ setIsModalVisible, isModalVisible }) => {
                 </List>
               </Box>
             </Box>}
-            <Box
-              sx={{
-                position: "absolute",
-                top:"50%",
-                left:0,
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between"
-              }}
-            >
-              <IconButton onClick={() => console.log()} sx={{transform: "translateX(-150px) translateY(-50%)"}}>
-                <ArrowBackIosNewIcon sx={navButtonProps}/>
-              </IconButton>
-              <IconButton onClick={() => console.log()} sx={{transform: "translateX(150px) translateY(-50%)"}}>
-                <ArrowForwardIosIcon sx={navButtonProps}/>
-              </IconButton>
-            </Box>
+
+            {/* project navigation buttons */}
+            {selectedImage &&<Portal container={() => document.getElementsByClassName('MuiModal-backdrop')[0]} >
+              <Box 
+                sx={{
+                  position: "fixed", 
+                  top:"50%", 
+                  left:0, 
+                  width: "100%", 
+                  display: "flex",
+                  justifyContent: "space-between",
+                  px: 5,
+                }}
+              >
+                <IconButton onClick={handleProjectBackClick} sx={{visibility: selectedImage.index === 0 ? "hidden" : "visible"}}>
+                  <ArrowBackIosNewIcon sx={navButtonProps}/>
+                </IconButton>
+                <IconButton onClick={handleProjectNextClick} sx={{visibility: selectedImage.index < projects.length - 1 ? "visible" : "hidden"}}>
+                  <ArrowForwardIosIcon sx={navButtonProps}/>
+                </IconButton>
+              </Box>
+            </Portal>}
           </Box>
         </Fade>
       </Modal>
