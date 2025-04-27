3206f2a136d624277900f0bdb17a82af05f297ef
paoloBugarin <paolopaolobugarin@gmail.com>
2025-04-27 16:00:57 -0700

    updated media queries

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PageContainer from '../../components/layout/PageContainer'
 import Sidebar from '../../components/layout/Sidebar/Sidebar';
 import Content from './content/Content';
-
+import MobileSidebar from '../../components/layout/Sidebar/MobileSidebar/MobileSidebar';
 
 const About = () => {
   window.scrollTo(0, 0);
@@ -10,6 +10,7 @@ const About = () => {
   return (
     <PageContainer  gridArea="about">
       <Sidebar/>
+      <MobileSidebar/>
       <Content  gridArea="about"/>
     </PageContainer>
   )
--- src/pages/about/content/Header.jsx
+++ src/pages/about/content/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'mui-image'
+import { useNavigate } from 'react-router'
 import { Box, Button, Typography } from '@mui/material'
 import caloyface from "../../../assets/caloyface.webp"
 import HandshakeIcon from '@mui/icons-material/Handshake';
@@ -9,10 +10,11 @@ import DescriptionIcon from '@mui/icons-material/Description';
 const containerProps = {
   // background: "rgba(60,60,60,.25)",
   display: "flex",
-  alignItems: "flex-start",
+  alignItems: {xs: "center", sm:"flex-start"},
   justifyContent: "center",
-  gap: {lg: 15},
-  pt: 25,
+  flexDirection: {xs:"column-reverse", sm: "row"},
+  gap: {xs: 15, sm: 8, md: 12, lg: 15},
+  pt: {xs: 8, sm:25},
   pb: 18,
   minHeight: "65vh"
 }
@@ -25,14 +27,16 @@ const buttonContainerProps = {
 
 const buttonProps = {
   px:4,
-  fontSize: 16,
+  fontSize: {xs: 13, sm: 14, md: 15, lg: 16},
   letterSpacing: 1
 }
 
 const imageContainerProps = { 
   width: {
-    md: 60, 
-    lg: 65, 
+    xs: 140,
+    sm: 150,
+    md: 180, 
+    lg: 200, 
     xl: 230
   }, 
   display: "flex", 
@@ -44,17 +48,17 @@ const imageContainerProps = {
 const textContainerProps = {
   display:"flex",
   flexDirection: "column",
-  maxWidth: {xs: "100%", lg:"45%"}
+  maxWidth: {xs: "90%", sm: "70%", md: "60%", lg:"45%"}
 }
 const titleTextProps = {
-  fontSize: 40,
+  fontSize: {xs: 30, sm: 33, md: 35, lg: 40},
   fontWeight: 600,
   letterSpacing: 1.2,
   mb: 3,
 }
 
 const descTextProps = {
-  fontSize: 13,
+  fontSize: {xs: 12.5, md: 13, lg: 13},
   lineHeight: 1.8,
   opacity: .9,
   fontWeight: 200,
@@ -63,14 +67,16 @@ const descTextProps = {
 
 
 const Header = ({ professionalSummary, resume }) => {
+  const navigate = useNavigate();
+
   return (
     <Box sx={containerProps}>
       <Box sx={textContainerProps}>
         <Typography variant='h6' sx={titleTextProps}>I am Carl,</Typography>
         <Typography variant="h6" sx={descTextProps}>{professionalSummary}</Typography>
         <Box sx={buttonContainerProps}>
-          <Button variant="contained" sx={buttonProps} startIcon={<HandshakeIcon/>} color="primary">Hire me</Button>
-          <Button variant="outlined" sx={buttonProps} startIcon={<DescriptionIcon/>} color='info'>My CV</Button>
+          <Button variant="contained" sx={buttonProps} startIcon={<HandshakeIcon/>} color="primary" onClick={() => navigate("/contact")}>Hire me</Button>
+          <Button variant="outlined" sx={buttonProps} startIcon={<DescriptionIcon/>} color='info' href={resume.src} target='_blank'>My CV</Button>
         </Box>
       </Box>
       <Box sx={imageContainerProps}>
