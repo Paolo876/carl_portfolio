64f8af107d9fd32a26c1a8cabe7216e5fc22ad36
paoloBugarin <paolopaolobugarin@gmail.com>
2025-05-29 21:12:04 -0700

    updated login page

--- src/pages/dev/Login.jsx
+++ src/pages/dev/Login.jsx
@@ -1,15 +1,45 @@
 import { useGoogleLogin } from "../../hooks/useGoogleLogin";
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useNavigate } from 'react-router';
-import { Box, Container, Paper, Typography } from "@mui/material";
-import React from 'react'
+import { Box, Container, Paper, Typography, Button } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import GoogleIcon from '@mui/icons-material/Google';
+
+const containerProps = {
+  my: 12,
+  px: 5,
+  py: 8, 
+  overflow: "auto",
+}
+
+const headerTextProps = {
+	fontSize: {xs: 32, sm: 36, md: 38, lg: 40},
+	fontWeight: 700,
+	letterSpacing: 1.4, 
+	opacity: .9,
+	pl: {xs: 2, sm: 0},
+}
+
+const descTextProps = {
+  fontSize: {xs: 12, sm: 13, md: 14, lg:15},
+  lineHeight: 2.5,
+  fontWeight: 200,
+  letterSpacing: 1,
+  mt: 8
+}
+
+const buttonContainerProps = {
+  display: "flex",
+  flexDirection: {xs: "column", sm: "row"},
+  gap: 5,
+  mt: 8
+}
 
 const Login = () => {
   const navigate = useNavigate();
   const { user } = useAuthContext();
   const { login, isLoading } = useGoogleLogin();
 
-  // if(user) navigate('/');
   const handleLogin = async () => {
     await login();
   };
@@ -18,16 +48,34 @@ const Login = () => {
 
   return (
     <Container>
-      <Paper>
+      <Paper sx={containerProps}>
         <Box>
-          <Typography>LOGIN</Typography>
+          <Typography sx={headerTextProps} variant="h6">LOGIN</Typography>
         </Box>
         <Box>
-          <Typography>This page is reserved for the rightful owner of this site, Carl Adriant Dimabuyu.</Typography>
+          <Typography sx={descTextProps} variant="h6">This page is reserved for the rightful owner of this site, Carl Adriant Dimabuyu.</Typography>
         </Box>
-        <Box>
-          <Button disabled={isLoading}>Back To Home Page</Button>
-          <Button disabled={isLoading}>Login With Google</Button>
+        <Box sx={buttonContainerProps}>
+          <Button 
+            disabled={isLoading} 
+            onClick={handleLogin}
+            variant="contained"
+            color="primary"
+            startIcon={<GoogleIcon/>}
+            size="large"
+          >
+            Login With Google
+          </Button>
+          <Button 
+            disabled={isLoading} 
+            onClick={() => navigate("/")}
+            variant="outlined"
+            color="secondary"
+            startIcon={<HomeIcon/>}
+            size="large"
+          >
+            Back To Home Page
+          </Button>
         </Box>
       </Paper>
     </Container>
