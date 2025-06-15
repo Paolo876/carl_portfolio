92f88659c1907270b81e2afffe98aef9b34c2073
paoloBugarin <paolopaolobugarin@gmail.com>
2025-06-14 19:30:44 -0700

    updated ListInput component

--- src/components/FormInputs/ListInput.jsx
+++ src/components/FormInputs/ListInput.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import { useState } from 'react'
+import { TextField } from '@mui/material'
 
-const ListInput = () => {
+
+const inputStyles = {
+  sx: {
+    fontSize: { xs: 11, sm: 14.5, md: 15, lg: 15, xl:15 },
+    fontWeight: 500,
+    letterSpacing: 1.4,
+    lineHeight: 1.25, 
+    textShadow: "1px 1px 5px rgba(10,10,10,.75)",
+    "&:hover": {
+      textShadow: "1px 1px 3px rgba(100,100,100,.75)",
+    },
+  }
+}
+
+const inputLabelStyles = {
+  sx: {
+    fontSize: { xs: 11.5, sm: 15, md: 15, lg: 15, xl:15 },
+    fontWeight: 200,
+    opacity: .75,
+    letterSpacing: .75,
+    textShadow: "1px 1px 5px rgba(10,10,10,.75)",
+  }
+}
+
+
+const ListInput = ({ id, label, addValues, disabled=false }) => {
+  const [ input, setInput ] = useState("");
+
+  const handleChange = (e) => {
+    if (e.key === "Enter" && input.trim().length !== 0) {
+      addValues(prevState => [...prevState, input])
+      setInput("")
+    }
+  }
+
+
   return (
-    <div>ListInput</div>
+    <TextField
+      variant="outlined" 
+      fullWidth 
+      type="text" 
+      InputProps={inputStyles} 
+      InputLabelProps={inputLabelStyles} 
+      inputProps={{maxLength: 40}} 
+      id={id}
+      label={label}
+      value={input}
+      onKeyDown={e => handleChange(e)}
+      onChange={e => setInput(e.target.value)}
+      disabled={disabled}
+    />
   )
 }
 
--- src/pages/dev/newPost/PostInformationForm.jsx
+++ src/pages/dev/newPost/PostInformationForm.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Box, Typography, TextField, Chip, Button } from '@mui/material'
 import TextInput from '../../../components/FormInputs/TextInput'
 import AddIcon from '@mui/icons-material/Add';
+import ListInput from '../../../components/FormInputs/ListInput';
 
 
 const mainContainerProps = {
@@ -36,13 +37,28 @@ const softwaresInputContainerProps = {
   gap : 1,
 }
 
+const skillsListContainer = {
+  display: "flex",
+  gap: 1,
+  mt: 1.5
+}
+
+
+const MOCK_SKILLS = [ "Adobe Photoshop", "3DSMax", "Corona" ]
+
 
 const PostInformationForm = () => {
   const [ isLoading, setIsLoading ] = useState(false)
   const [ header, setHeader ] = useState("");
   const [ style, setStyle ] = useState("");
+  const [ softwares, setSoftwares ] = useState(MOCK_SKILLS)
   const [ softwareInput, setSoftwareInput ] = useState("");
 
+
+  const handleDeleteSkill = (item) => {
+    setSoftwares(prevState => prevState.filter(_item => _item !== item))
+  }
+  console.log(softwares)
   return (
     <Box sx={mainContainerProps}>
       <Box sx={headerTextContainerProps}>
@@ -70,15 +86,12 @@ const PostInformationForm = () => {
         <Box sx={formItemProps}>
           <Typography sx={formHeaderTextProps} variant="h6">Softwares used</Typography>
           <Box sx={softwaresInputContainerProps}>
-            {/* <TextInput
-              id="softwaresInput" 
-              label="Adobe Photoshop"
-              setValue={value => setSoftwareInput(value)}
-              disabled={isLoading}
-            /> */}
+            <ListInput addValues={setSoftwares}/>
             <Button variant="contained" endIcon={<AddIcon/>}>Add</Button>
           </Box>
-
+          <Box sx={skillsListContainer}>
+            { softwares.map(item => <Chip label={item} variant="outlined" onDelete={() => handleDeleteSkill(item)} key={item} color="success"/>) }
+          </Box>
         </Box>
         {/* softwares used - fetch softwares from existing projects */}
       </Box>
