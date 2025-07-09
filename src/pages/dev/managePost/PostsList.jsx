3fe95c3e035e40e1ff04ee05be290a71d118f35d
paoloBugarin <paolopaolobugarin@gmail.com>
2025-07-08 21:34:22 -0700

    updated EditPosts components

--- src/pages/dev/managePost/PostsList.jsx
+++ src/pages/dev/managePost/PostsList.jsx
@@ -10,15 +10,14 @@ import DeleteIcon from '@mui/icons-material/Delete';
 const containerProps = {
   borderTop: 1,
   borderColor: "rgba(50,50,50,.85)",
-  mt: {md: 3},
-  pt: {md: 3},
+  mt: 3,
+  pt: 3,
 }
-
 
 const projectImagesContainerProps = {
   pt: 5,
   display: "grid",
-  gap: 1,
+  gap: 2,
   height: "100%",
   width: "100%",
   gridTemplateColumns: {
@@ -26,9 +25,6 @@ const projectImagesContainerProps = {
     sm: `65vw`,
     md: `repeat(auto-fill, minmax(250px, 1fr))`,
     lg: `repeat(auto-fill, minmax(350px, 1fr))`
-    // md: `repeat(auto-fill, minmax(${isSidebarCollapsed.state ? "270px": "220px"}, 1fr))`,
-    // lg: `repeat(auto-fill, minmax(${isSidebarCollapsed.state ? "300px": "250px"}, 1fr))`,
-    // xl: `repeat(auto-fill, minmax(${isSidebarCollapsed.state ? "350px": "280px"}, 1fr))`
   },
   justifyContent: "center"
 }
@@ -37,26 +33,26 @@ const boxContainerProps = {
   border: 1, 
   borderColor: "rgba(255,255,255,.1)", 
   height: {md: 240, lg: 240,  xl: 275},
-  // cursor: "pointer",
   position: "relative",
   overflow: "hidden",
   display: "flex",
   flexDirection: "column"
-  // transition: "300ms width ease",
-  // display: {xs: "none", md: "initial"}
 }
 
 
 const informationContainer = {
-  // position: "absolute",
-  // top: 0,
-  // left: 0,
-  // height: "100%",
-  // width: "100%",
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
-  pl: 1,
+  pl: .5,
+  mb: 2,
+}
+
+const headerTextProps = {
+  fontSize: { xs: 14, sm: 14, lg:15 },
+}
+const actionButtonsContainer = {
+  display : "flex",
 }
 
 
@@ -76,6 +72,15 @@ const PostsList = () => {
     return { header: item.header, coverImage: {filename:item.images[0].filename, src: newStr}, id: item.id }
   })
 
+  const handleClick = (action) => {
+    if(action === "edit") {
+
+    }
+    if(action === "delete") {
+
+    }
+  }
+
 
   return (
     <Box sx={containerProps}>
@@ -86,10 +91,10 @@ const PostsList = () => {
             <Image src={item.coverImage.src}  duration={250} sx={{transition: "300ms width ease"}} fit='cover' showLoading easing="ease-in-out"/>
           </Box>
           <Box sx={informationContainer}>
-            <Typography>{item.header}</Typography>
-            <Box>
-              <IconButton size="large" color='primary'><EditIcon/></IconButton>
-              <IconButton size="large" color='error'><DeleteIcon/></IconButton>
+            <Typography variant='h6' sx={headerTextProps}>{item.header}</Typography>
+            <Box sx={actionButtonsContainer}>
+              <IconButton color='primary' onClick={() => handleClick("edit")}><EditIcon/></IconButton>
+              <IconButton color='secondary' onClick={() => handleClick("delete")}><DeleteIcon/></IconButton>
             </Box>
           </Box>
         </Box>)}
