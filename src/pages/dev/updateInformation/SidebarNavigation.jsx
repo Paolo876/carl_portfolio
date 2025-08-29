4ed2ecaa9a62076f5d85a49c66de45f9d9b8a1a9
paoloBugarin <paolopaolobugarin@gmail.com>
2025-08-29 00:38:10 -0700

    updated UpdateInformation component

---
+++ src/pages/dev/updateInformation/SidebarNavigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { List, ListItem, ListItemButton } from '@mui/material'
+
+
+const SidebarNavigation = () => {
+  return (
+    <List>
+        <ListItemButton>One</ListItemButton>
+        <ListItemButton>One</ListItemButton>
+        <ListItemButton>One</ListItemButton>
+        <ListItemButton>One</ListItemButton>
+    </List>
+  )
+}
+
+export default SidebarNavigation
--- src/pages/dev/updateInformation/UpdateInformation.jsx
+++ src/pages/dev/updateInformation/UpdateInformation.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material'
+import DevPageContainer from '../../../components/layout/DevPageContainer'
+import SidebarNavigation from './SidebarNavigation'
+import Content from './Content'
+
+const mainContainerProps = {
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  minHeight: "70vh"
+}
+
+const headerTextContainerProps = {
+  mt: {xs: 1, sm: 2, md: 2},
+  mb: {xs: 5, sm: 8, md: 8, lg: 8}
+
+}
+const headerTextProps = {
+  fontSize: {xs: 20, sm: 20, md: 26, lg: 30},
+  fontWeight: 600,
+  letterSpacing: 1.5,
+  textDecoration: "underline"
+}
+
+const mainContent = {
+
+}
+
+const navigationOptions = [ 
+  {id :"careerObjective", title: "Career Objective"},
+  {id :"experience", title: "Work Experiences"},
+  {id :"information", title: "Personal Information"},
+  {id :"professionalSummary", title: "Professional Summary"},
+  {id :"resume", title: "Resume"},
+  {id :"softwareSkills", title: "Software Skills"},
+]
 
 const UpdateInformation = () => {
   return (
-    <div>UpdateInformation</div>
+    <DevPageContainer>
+      <Box sx={mainContainerProps}>
+        <Box sx={headerTextContainerProps}>
+          <Typography variant="h6" sx={headerTextProps}>Update Information</Typography>
+        </Box>
+        <Box sx={mainContent}>
+          <SidebarNavigation navigationOptions={navigationOptions}/>
+          <Content/>
+        </Box>
+      </Box>
+    </DevPageContainer>
   )
 }
 
