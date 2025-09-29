98694f38e2038ef53e875a60e7eaaca74cbcdfda
paoloBugarin <paolopaolobugarin@gmail.com>
2025-09-29 00:22:27 -0700

    updated Software Skills component

--- src/pages/dev/updateInformation/Content/SoftwareSkills.jsx
+++ src/pages/dev/updateInformation/Content/SoftwareSkills.jsx
@@ -3,10 +3,25 @@ import ContentItemContainer from './ContentItemContainer'
 import useAboutRedux from '../../../../hooks/useAboutRedux'
 import { TextField, Alert, Box, List, ListItem, Typography, Button } from '@mui/material'
 import { useFirestore } from '../../../../hooks/useFirestore'
+import objectDeepCompare from '../../../../utils/objectDeepCompare'
 import Image from 'mui-image'
+import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
 
+const listItemProps = {
+  display: "flex",
+  justifyContent: "space-between",
+  backgroundColor: "rgba(255,255,255,.1)",
+  borderRadius: 3,
+  mb: 2,
+}
 
-
+const listItemContentProps = {
+  display: "flex",
+  alignItems: "center",
+  flex: 1
+}
 const textProps = {
   fontWeight: 400,
   letterSpacing: 1.25,
@@ -17,17 +32,19 @@ const textProps = {
 const SoftwareSkills = ({ id, title }) => {
   const { about, updateInfo } = useAboutRedux();
   const { updateDocument } = useFirestore("user");
-  const [ inputValue, setInputValue ] = useState(about.careerObjective)
+  const [ updatedSkills, setUpdatedSkills ] = useState(about.softwareSkills)
   const [ isLoading, setIsLoading ] = useState(false)
   const [ error, setError ] = useState(null);
+  const [ isInputVisible, setIsInputVisible ] = useState(false);
+
 
   const handleSubmit = async () => {
     setIsLoading(true)
     try {
       // update document
-      await updateDocument({...about, careerObjective: inputValue}, 'about');
+      // await updateDocument({...about, careerObjective: inputValue}, 'about');
       // update redux
-      updateInfo({careerObjective: inputValue})
+      // updateInfo({careerObjective: inputValue})
     } catch(err){
       console.log(err)
       setError(err.message);
@@ -37,22 +54,37 @@ const SoftwareSkills = ({ id, title }) => {
     }
   }
 
-  console.log(about.softwareSkills)
+  const handleEdit = (src) => {
+    
+  }
+
+  const handleDelete = (src) => {
+    setUpdatedSkills(prevState => prevState.filter(item => item.src !== src))
+  }
+
   return (
-    <ContentItemContainer id={id} title={title} isDisabled={isLoading || inputValue === about.careerObjective} onClick={handleSubmit}>
-      <Box>
+    <ContentItemContainer id={id} title={title} isDisabled={isLoading || objectDeepCompare(about.softwareSkills, updatedSkills)} onClick={handleSubmit}>
+      <Box sx={{mb: 3, borderBottom: 1, pb: 2, borderColor: "rgba(100,100,100,.1)"}}>
         {/* input here */}
+        {!isInputVisible && <Box>
+          <Button sx={{width: "100%", py: 1.75}} variant="contained" size="large" endIcon={<AddIcon/>} onClick={() => setIsInputVisible(true)}>Add New Skill</Button>
+        </Box>}
       </Box>
       <List>
-        {about.softwareSkills && about.softwareSkills.map(item => <List>
-          <ListItem key={item.name}>
-            <Box sx={{height: 30, width: 30, display: "flex", mr: 2}}>
-              <Image src={item.src} fit="scale-down" height="auto" width="auto" />
+        {updatedSkills && updatedSkills.map(item =>
+          <ListItem key={item.name} sx={listItemProps}>
+            <Box sx={listItemContentProps}>
+              <Box sx={{height: 30, width: 30, display: "flex", mr: 2}}>
+                <Image src={item.src} fit="scale-down" height="auto" width="auto" />
+              </Box>
+              <Typography variant='h6' sx={textProps}>{item.name}</Typography>
+            {/* <TextField fullWidth size='small'/> */}
             </Box>
-            <Typography variant='h6' sx={textProps}>{item.name}</Typography>
-            <Button>x</Button>
-          </ListItem>
-        </List>)}
+            <Box sx={{display: "flex", gap: 1}}>
+              <Button variant='outlined' color='info' onClick={() => handleEdit(item.src)}><EditIcon/></Button>
+              <Button variant='contained' color='secondary' onClick={() => handleDelete(item.src)}><CloseIcon/></Button>
+            </Box>
+          </ListItem>)}
       </List>
     </ContentItemContainer>
   )
